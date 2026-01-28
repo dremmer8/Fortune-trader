22261675b632c3e60b63804808c13a107949160d
Pavel Arkhipov <136486860+dremmer8@users.noreply.github.com>
2026-01-28 23:01:21 +0200

    singleplayer removed

--- singleplayer/js/version.js
+++
@@ -1,8 +0,0 @@
-// Version string for the deployed build.
-// Update this value manually when a new iteration is created/pushed to the repository.
-const APP_VERSION = 'v0.023';
-
-if (typeof window !== 'undefined') {
-    window.APP_VERSION = APP_VERSION;
-    window.dispatchEvent(new CustomEvent('app-version-ready', { detail: { version: APP_VERSION } }));
-}
