8841f0de07f4984498596bf17210a7e6de467840
Pavel Arkhipov <136486860+dremmer8@users.noreply.github.com>
2026-02-07 15:11:18 +0200

    update

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 // Version string for the deployed build.
 // Update this value manually when a new iteration is created/pushed to the repository.
-const APP_VERSION = 'v0.026';
+const APP_VERSION = 'v0.027';
 
 if (typeof window !== 'undefined') {
     window.APP_VERSION = APP_VERSION;
